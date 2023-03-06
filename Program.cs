98d2e4fc1a5d50aa6b3811358fb5b3d085dffd4e
ASUS <ASUS@LAPTOP-LLK82SRR>
2023-03-06 23:24:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+
+namespace hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sanjyot = 23;
+            // Console.WriteLine("hello world");
+            //Console.Write("hello sanjyot and the number" + sanjyot);
+            //string inp = Console.ReadLine();
+            //Console.WriteLine(inp);
+            int a =  34;
+            float b = 43.5F;
+            //Console.WriteLine(a + b);
+            //char s = 'a';
+            //Console.WriteLine(s);
+            int r = 5;
+            double k = r;
+            Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
